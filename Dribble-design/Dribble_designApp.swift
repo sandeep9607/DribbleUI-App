fac5a37d9f94ab4b5d49d02111bb7ac8c7f81c9e
swiftdev <swiftdev69@gmail.com>
2021-12-23 12:56:15 +0530

    initial project

---
+++ Dribble-design/Dribble_designApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dribble_designApp.swift
+//  Dribble-design
+//
+//  Created by Sam Maurya on 23/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Dribble_designApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
